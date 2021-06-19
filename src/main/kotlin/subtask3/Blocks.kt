83a528f66bec0e49db70ddb8a5b7dbcbd1ee77fe
Nikolai24 <61163795+Nikolai24@users.noreply.github.com>
2021-06-19 13:02:28 +0300

    Update Blocks.kt

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,49 @@
 package subtask3
 
 import kotlin.reflect.KClass
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.math.abs
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when (blockB) {
+            String::class -> {
+                var result = ""
+                for (i in blockA) {
+                    if (i is String) {
+                        result += i
+                    }
+                }
+                return result
+            }
+            Int::class -> {
+                var sum = 0
+                for (i in blockA) {
+                    if (i is Int) {
+                        sum += i
+                    }
+                }
+                return sum
+            }
+            else -> {
+                var date = LocalDate.now()
+                var result =  LocalDate.now()
+                var difference : Long
+                var check = Long.MAX_VALUE
+                for (i in blockA) {
+                    if (i is LocalDate){
+                        difference = abs(ChronoUnit.DAYS.between(i, date))
+                        if (difference < check){
+                            check = difference
+                            result = i
+                        }
+                    }
+                }
+                return result.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            }
+        }
     }
 }
